409059f91c357a94c60b9484819d6b7f5161eb9e
tirafesi <tirafesi@hotmail.com>
2016-10-14 19:09:03 +0100

    almost finished with printing the board

--- Board.pl
+++ Board.pl
@@ -1,25 +1,84 @@
-/* Board class */
-
-/* teste */
-teste:-  write(' _  _'), nl,
-            write('/ \\/ \\'), nl,
-            write(' 1  2'), nl,
-            write('\\-/\\-/').
-
-/*
-width: 5 spaces
-height: 3 lines
-how to draw: (sp = space, s = slash, bs = backslash, nl = newline, b = vertical bar)
-sp, s, sp, bs, nl
-b, sp, c, sp, b, nl
-sp, bs, sp, s
+/**
+Board is a 2D array (9x9!) (list of lists) that should ALWAYS be access like this:
+board[Y][X]
 */
 
-teste2:-    write('   / \\'), nl,
-            write('  | 3 |'), nl,
-            write(' / \\ /'), nl,
-            write('| 2 |'), nl,
-            write(' \\ /').
+/* Board radius N = max(abs(x), abs(z)) = 4  */
+boardRadius(4).
 
-/* hexagonal coordinates mapping array[R+N][Q+N+min(0, R)] N = max(abs(x), abs(z)) = 4 */
-hex(Q, R, X, Y):- X
+/**
+* Hexagonal to array coordinates mapping: array[R + N][Q + N + min(0, R)], N = board radius
+* @param Q Hexagonal coordinate (SW - NE)
+* @param R Hexagonal coordinate (N - S)
+* @param X Array coordinate (columns)
+* @param Y Array coordinate (rows)
+*/
+map(Q, R, X, Y):-
+  boardRadius(N), /* get board radius and store it in variable N */
+  Y is R + N,
+  X is Q + N + min(0, R).
+
+/* translation rules used for printing the board */
+toChar(emptyCell, ' ').
+toChar(blackPiece, 'B').
+toChar(whitePiece, 'W').
+
+/* board (2D array) measures */
+boardWidth(9).
+boardHeight(9).
+
+/* empty board */
+emptyBoard([
+[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, nullCell, nullCell, nullCell, nullCell],
+[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, nullCell, nullCell, nullCell],
+[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, nullCell, nullCell],
+[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, nullCell],
+[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, nullCell],
+[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, nullCell, nullCell],
+[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, nullCell, nullCell, nullCell],
+[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, nullCell, nullCell, nullCell, nullCell]]).
+
+/**
+* Prints the bottom of a cell to the terminal
+*/
+printBottom:- write('\\ /').
+
+/**
+* Prints one piece to the terminal
+* @param Piece Element of a line of the board to print (will call translation rules)
+* @param Index Set to '0' if it's the first element of a line.
+*/
+printPiece(Piece, 0):- write('|'), printPiece(Piece, 1).
+printPiece(Piece, _Index):-
+  toChar(Piece, C), /* char C to print */
+  write(' '),
+  write(C),
+  write(' |').
+
+
+/**
+* Prints one line of the board (a list) to the terminal
+* @param [Piece|Tail] List to print
+* @param LineIndex Index of the current line being iterated (range: [0, board.height])
+* @param ElemIndex Index of the current line element being iterated (range: [0, board.width])
+* @param Type Either one of these values: top, middle, bottom.
+*/
+printLine([Piece|Tail], LineIndex, ElemIndex, middle):-
+  boardWidth(Width),          /* get board width */
+  Index > -1, Index < Width,  /* while (i >= 0 && i < board.height) */
+  printPiece(Piece, Index),   /* print char */
+  NewIndex is Index + 1,      /* i++ */
+  printLine(Tail, NewIndex).  /* recursive call */
+
+/**
+* Prints Board to the terminal
+* @param Line|Tail] Board to print - 2D Array (list of lists) representing the game board
+* @param Index Index of the current line being iterated (range: [0, board.height])
+*/
+printBoard([Line|Tail], Index):-
+  boardHeight(Height),        /* get board height */
+  Index > -1, Index < Height,  /* while (i >= 0 && i < board.height) */
+  printLine(Line, Index, middle),   /* print char */
+  NewIndex is Index + 1,      /* i++ */
+  printLine(Tail, NewIndex).  /* recursive call */
---
+++ teste.pl
@@ -0,0 +1,22 @@
+/* teste */
+teste:-
+  write(' _  _'), nl,
+  write('/ \\/ \\'), nl,
+  write(' 1  2'), nl,
+  write('\\-/\\-/').
+
+/*
+width: 5 spaces
+height: 3 lines
+how to draw: (sp = space, s = slash, bs = backslash, nl = newline, b = vertical bar)
+sp, s, sp, bs, nl
+b, sp, c, sp, b, nl
+sp, bs, sp, s
+*/
+
+teste2:-
+  write('   / \\'), nl,
+  write('  | 3 |'), nl,
+  write(' / \\ /'), nl,
+  write('| 2 |'), nl,
+  write(' \\ /').
