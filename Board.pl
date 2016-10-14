11481c75fbca56c97a15b97eced8db1f0510b9b9
tirafesi <tirafesi@hotmail.com>
2016-10-14 15:44:50 +0100

    creating rep

---
+++ Board.pl
@@ -0,0 +1,25 @@
+/* Board class */
+
+/* teste */
+teste:-  write(' _  _'), nl,
+            write('/ \\/ \\'), nl,
+            write(' 1  2'), nl,
+            write('\\-/\\-/').
+
+/*
+width: 5 spaces
+height: 3 lines
+how to draw: (sp = space, s = slash, bs = backslash, nl = newline, b = vertical bar)
+sp, s, sp, bs, nl
+b, sp, c, sp, b, nl
+sp, bs, sp, s
+*/
+
+teste2:-    write('   / \\'), nl,
+            write('  | 3 |'), nl,
+            write(' / \\ /'), nl,
+            write('| 2 |'), nl,
+            write(' \\ /').
+
+/* hexagonal coordinates mapping array[R+N][Q+N+min(0, R)] N = max(abs(x), abs(z)) = 4 */
+hex(Q, R, X, Y):- X
